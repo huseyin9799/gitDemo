98838db4bf2ed2242fb36aacae722dfbd6cf06ce
huseyindemirharmony <huseyin.demir.harmony@gmail.com>
2020-08-03 16:22:56 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,10 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+
+        System.out.print("Previous batches: ");
+        for(int i=1; i<=19; i++){
+            System.out.print(i+" ");
+        }
     }
 }
